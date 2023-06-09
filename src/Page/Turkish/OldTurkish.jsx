27d01c5abdd165dcb777e427f3bc9a5c51aad5f0
MidNightRider666 <ledas850@gmail.com>
2023-06-09 11:45:55 +0300

    Some Changes

---
+++ src/Page/Turkish/OldTurkish.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useRef } from "react";
+import SecondNav from "../../Components/SecondNav/SecondNav";
+import css from "./OldTurkish.module.scss";
+
+
+
+
+function OldTurkish() {
+
+
+  const headerRef = useRef();
+  const headerRef1 = useRef();
+  function ScrollHeight() {
+    var content = headerRef.current
+    var container = headerRef1.current
+
+    content.style.height = container.offsetHeight + 'px';
+  }
+
+
+  useEffect(()=>{
+    ScrollHeight()
+    }, [])
+
+
+  return (
+    <div>
+    <div className={css.body}>
+    <main>
+      <div ref={headerRef} id={css.parchment}></div>
+      <div ref={headerRef1} id={css.contain}>
+        <p>
+        Turkiškos pirties istorija siekia labai tolimus laikus, kai turkai dar nebuvo pasiekę Antalijos. Atvykę į Antaliją jie su savimi atsinešė senovines tradicijas, o taip pat susidūrė su kita tradicija – romų ir Bizantijos. Šių dviejų tradicijų mišinys leido atsirasti naujai Turkiškos pirties koncepcijai. Iš pradžių pirtis iš apačios būdavo šildoma šiltu oru, kuris patekdavo per specialius vamzdžius, kurie būdavo paslėpti po marmurinėmis pirties grindimis. Be to turkiška pirtis dar būdavo kaitinama didžiulio katilo pagalba, kuris būdavo įmontuojamas į sieną. Šis katilas atliko garų gamintojo vaidmenį. Vanduo katile virdavo (po juo būdavo krosnis), o susidarę garai patekdavo pro pakankamai nemažą angą, esančią maždaug pusantro metro aukštyje.
+        </p>
+        <img className={css.image} src="https://blog.thermasol.com/wp-content/uploads/2019/02/1-header-3.jpg" alt="" />
+      </div>
+      </main>
+      <svg>
+        <filter id="wavy2">
+          <feTurbulence
+            x="0"
+            y="0"
+            baseFrequency="0.02"
+            numOctaves="5"
+            seed="1"
+          />
+          <feDisplacementMap in="SourceGraphic" scale="20" />
+        </filter>
+      </svg>
+      </div>
+    </div>
+  );
+
+  // return (
+  //   <div>
+  //     <SecondNav/>
+  //     <h1 className={css.title}>Turkish Bath</h1>
+  //     <div className={css.gallery}>
+  //     <img className={css} src="https://lisamora.lt/wp-content/uploads/2015/12/3.17-1024x680.jpg" alt="" />
+  //     <img className={css} src="https://blog.thermasol.com/wp-content/uploads/2019/02/1-header-3.jpg" alt="" />
+  //     </div>
+  //     <hr className={css.firstHr} />
+  //     <div className={css.wrap}>
+  //       <div className={css.box}>
+  //     <h1 className={css.firstParaTitle}>The old ways</h1>
+  //     <p className={css.firstPara}>Turkiškos pirties istorija siekia labai tolimus laikus, kai turkai dar nebuvo pasiekę Antalijos. Atvykę į Antaliją jie su savimi atsinešė senovines tradicijas, o taip pat susidūrė su kita tradicija – romų ir Bizantijos. Šių dviejų tradicijų mišinys leido atsirasti naujai Turkiškos pirties koncepcijai. Iš pradžių pirtis iš apačios būdavo šildoma šiltu oru, kuris patekdavo per specialius vamzdžius, kurie būdavo paslėpti po marmurinėmis pirties grindimis. Be to turkiška pirtis dar būdavo kaitinama didžiulio katilo pagalba, kuris būdavo įmontuojamas į sieną. Šis katilas atliko garų gamintojo vaidmenį. Vanduo katile virdavo (po juo būdavo krosnis), o susidarę garai patekdavo pro pakankamai nemažą angą, esančią maždaug pusantro metro aukštyje.</p>
+  //     </div>
+  //     <img className={css.oldBath} src="https://blog.thermasol.com/wp-content/uploads/2019/02/1-header-3.jpg" alt="" />
+  //     </div>
+  //     <div className={css.wrap}>
+  //       <div className={css.box}>
+  //     <h1 className={css.firstParaTitle}>The new ways</h1>
+  //     <p className={css.firstPara}>Dabar turkiškos pirtys yra „sluoksniuoto torto“ tipo, susideda iš kokybiškos (dvisluoksnės) hidroizoliacijos, dvigubo šildymo, kaitinimo sistemos vandeniu arba elektra, paslėpto gelžbetoninėse konstrukcijose. Klasikinės lubos – kupolo formos, kad nutekėtų susidaręs kondensatas, kupolo apačioje yra pravedamas drenažas. Didelė drėgmė viduje pasiekiama garų generatoriaus dėka, aromatus pirtyje generuoja aromatų dozatorius. Apšvietimas tiekiamas šilumai atspariomis, nuo drėgmės apsaugotomis lemputėmis (įgaunamas žvaigždėto dangaus efektas). Privalomai turi būti atvestos ventiliacijos ir kanalizacijos sistemos. Vidinis dizainas gali būti įvairus: europietiškas – mozaika, dekoratyvinės plytelės, neoninis apšvietimas, klasikinis – marmuras, kolonos, piliastrai, mozaikos gultas, dubenys apsiprausimui, fontanai.</p>
+  //     </div>
+  //     <img className={css.oldBath} src="https://lisamora.lt/wp-content/uploads/2015/12/3.17-1024x680.jpg" alt="" />
+  //     </div>
+  //   </div>
+  // )
+}
+
+export default OldTurkish;
--- src/Page/Turkish/Turkish.jsx
+++ src/Page/Turkish/Turkish.jsx
@@ -1,37 +1,46 @@
-import React from 'react'
-import SecondNav from '../../Components/SecondNav/SecondNav'
-import css from './Turkish.module.scss'
+import React, { useEffect, useRef } from "react";
+import SecondNav from "../../Components/SecondNav/SecondNav";
+import css from "./Turkish.module.scss";
+import OldTurkish from "./OldTurkish";
+import NewTurkish from "./NewTurkish";
+import { useState } from "react";
+
+
+
 
 function Turkish() {
 
+    const [active, setActive] = useState(1);
+  const SetView = (active) => {
+    setActive(active);
+  };
 
+  const ActiveView = () => {
+    switch (active) {
+      case 1:
+        return <OldTurkish />;
+      case 2:
+        return <NewTurkish />;
+    }
+  };
+
+  console.log("actvie===", active);
 
 
   return (
     <div>
-      <SecondNav/>
-      <h1 className={css.title}>Turkish Bath</h1>
-      <hr className={css.firstHr} />
-      <div className={css.gallery}>
-      <img className={css} src="https://lisamora.lt/wp-content/uploads/2015/12/3.17-1024x680.jpg" alt="" />
-      <img className={css} src="https://blog.thermasol.com/wp-content/uploads/2019/02/1-header-3.jpg" alt="" />
-      </div>
-      <div className={css.wrap}>
-        <div className={css.box}>
-      <h1 className={css.firstParaTitle}>The old ways</h1>
-      <p className={css.firstPara}>Turkiškos pirties istorija siekia labai tolimus laikus, kai turkai dar nebuvo pasiekę Antalijos. Atvykę į Antaliją jie su savimi atsinešė senovines tradicijas, o taip pat susidūrė su kita tradicija – romų ir Bizantijos. Šių dviejų tradicijų mišinys leido atsirasti naujai Turkiškos pirties koncepcijai. Iš pradžių pirtis iš apačios būdavo šildoma šiltu oru, kuris patekdavo per specialius vamzdžius, kurie būdavo paslėpti po marmurinėmis pirties grindimis. Be to turkiška pirtis dar būdavo kaitinama didžiulio katilo pagalba, kuris būdavo įmontuojamas į sieną. Šis katilas atliko garų gamintojo vaidmenį. Vanduo katile virdavo (po juo būdavo krosnis), o susidarę garai patekdavo pro pakankamai nemažą angą, esančią maždaug pusantro metro aukštyje.</p>
-      </div>
-      <img className={css.oldBath} src="https://blog.thermasol.com/wp-content/uploads/2019/02/1-header-3.jpg" alt="" />
-      </div>
-      <div className={css.wrap}>
-        <div className={css.box}>
-      <h1 className={css.firstParaTitle}>The new ways</h1>
-      <p className={css.firstPara}>Dabar turkiškos pirtys yra „sluoksniuoto torto“ tipo, susideda iš kokybiškos (dvisluoksnės) hidroizoliacijos, dvigubo šildymo, kaitinimo sistemos vandeniu arba elektra, paslėpto gelžbetoninėse konstrukcijose. Klasikinės lubos – kupolo formos, kad nutekėtų susidaręs kondensatas, kupolo apačioje yra pravedamas drenažas. Didelė drėgmė viduje pasiekiama garų generatoriaus dėka, aromatus pirtyje generuoja aromatų dozatorius. Apšvietimas tiekiamas šilumai atspariomis, nuo drėgmės apsaugotomis lemputėmis (įgaunamas žvaigždėto dangaus efektas). Privalomai turi būti atvestos ventiliacijos ir kanalizacijos sistemos. Vidinis dizainas gali būti įvairus: europietiškas – mozaika, dekoratyvinės plytelės, neoninis apšvietimas, klasikinis – marmuras, kolonos, piliastrai, mozaikos gultas, dubenys apsiprausimui, fontanai.</p>
-      </div>
-      <img className={css.oldBath} src="https://lisamora.lt/wp-content/uploads/2015/12/3.17-1024x680.jpg" alt="" />
-      </div>
+    <SecondNav/>
+    <h1 className={css.title}>Turkish Bath</h1>
+    <div className={css.gallery}>
+    <img onClick={() => SetView(2)} className={css} src="https://lisamora.lt/wp-content/uploads/2015/12/3.17-1024x680.jpg" alt="" />
+    <img onClick={() => SetView(1)} className={css} src="https://blog.thermasol.com/wp-content/uploads/2019/02/1-header-3.jpg" alt="" />
     </div>
-  )
+    {ActiveView()}
+    </div>
+  );
+
 }
 
-export default Turkish
+  
+
+export default Turkish;
